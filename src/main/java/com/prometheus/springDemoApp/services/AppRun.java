ac8305b8fb2b7b1a671856fa8529e1619702a50e
Martin <martin1.bugar@gmail.com>
2020-09-05 14:21:00 +0200

    Dependency injection - typy

--- src/main/java/com/prometheus/springDemoApp/services/AppRun.java
+++ src/main/java/com/prometheus/springDemoApp/services/AppRun.java
@@ -6,8 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppRun {
 
-    @Autowired
+    //    @Autowired  1. sposob dependency injection
     MovieService movieService;
+
+//    @Autowired // ked je tam tato anotacia nemusi sa robit dependency injection cez konstruktor, ale moze sa... 2. sposob dependency injection
+//    public AppRun (MovieService movieService){
+//        this.movieService = movieService;
+//    }
+
+    @Autowired // 3. sposob dependency injection
+    public void setMovieService(MovieService movieService){
+        this.movieService = movieService;
+    }
+
 
     public void run(){
         movieService.createAndAddMovie();
