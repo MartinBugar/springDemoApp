581ed00a1df765871020d6ae6e3d2e21fc679e29
Martin <martin1.bugar@gmail.com>
2020-09-05 15:16:04 +0200

    Profiles

--- src/main/java/com/prometheus/springDemoApp/services/servicesImpl/PrimaryHelloWorld.java
+++ src/main/java/com/prometheus/springDemoApp/services/servicesImpl/PrimaryHelloWorld.java
@@ -2,10 +2,12 @@ package com.prometheus.springDemoApp.services.servicesImpl;
 
 import com.prometheus.springDemoApp.services.HelloWorldService;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
+@Profile("eng")
 public class PrimaryHelloWorld implements HelloWorldService {
     @Override
     public void sayHello() {
--- src/main/java/com/prometheus/springDemoApp/services/servicesImpl/SlovakHelloWorld.java
+++ src/main/java/com/prometheus/springDemoApp/services/servicesImpl/SlovakHelloWorld.java
@@ -1,9 +1,11 @@
 package com.prometheus.springDemoApp.services.servicesImpl;
 
 import com.prometheus.springDemoApp.services.HelloWorldService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("eng")
 public class SlovakHelloWorld implements HelloWorldService {
     @Override
     public void sayHello() {
