4152b5b6f5a20644b4090fb16ed4f0ef23294e10
MartinBugar <martin1.bugar@gmail.com>
2020-09-10 10:37:16 +0200

    REST API

--- src/main/java/com/prometheus/springDemoApp/restApi/MovieController.java
+++ src/main/java/com/prometheus/springDemoApp/restApi/MovieController.java
@@ -1,8 +1,11 @@
 package com.prometheus.springDemoApp.restApi;
 
 import com.prometheus.springDemoApp.model.Movie;
+import com.prometheus.springDemoApp.model.dto.MovieDTO;
 import com.prometheus.springDemoApp.repositories.MovieRepository;
+import com.prometheus.springDemoApp.services.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +14,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MovieController {
-   MovieRepository movieRepository;
 
-    public MovieController(MovieRepository movieRepository){
-        this.movieRepository = movieRepository;
+    MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
     }
 
     @GetMapping("/movies")
-    List<Movie> getAll (){
-        return movieRepository.findAll();
+    List<MovieDTO> all(){
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/movies/{id}")
+    MovieDTO getMovieById(@PathVariable("id") long movieId){
+        return movieService.getMovieById(movieId);
     }
 }
